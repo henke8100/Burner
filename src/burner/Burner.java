d71525c844d981ac34d1c3c4aaf25fc022938fc0
henke8100 <hk.henrik@gmail.com>
2011-04-30 17:40:42 +0200

    Edited

--- src/burner/Burner.java
+++ src/burner/Burner.java
@@ -1,5 +1,27 @@
 package burner;
 
+import java.util.Scanner;
+
 public class Burner {
 
+   public static void main(String[] args) {
+
+       String name;
+       int age;
+       Scanner in = new Scanner(System.in);
+
+       // Reads a single line from the console 
+       // and stores into name variable
+       name = in.nextLine();
+
+       // Reads a integer from the console
+       // and stores into age variable
+       age=in.nextInt();
+       in.close();            
+       
+       // Prints name and age to the console
+       System.out.println("Name :"+name);
+       System.out.println("Age :"+age);
+
+    }
 }
